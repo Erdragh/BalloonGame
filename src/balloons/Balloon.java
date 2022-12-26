b91a0b000368625d86ff52649d5494baf765a53b
Jan Bayer <enderjanyt@gmail.com>
2022-12-26 14:34:55 +0100

    Part6 - Animating Balloons

--- src/balloons/Balloon.java
+++ src/balloons/Balloon.java
@@ -4,12 +4,14 @@ import gdi.game.sprite.AbstractSpriteWorld;
 import gdi.game.sprite.Sprite;
 
 import java.awt.*;
-import java.util.Random;
 
 public class Balloon extends Sprite {
     private final int DIAMETER = 100;
+    private final int SPEED = 100;
 
     private final Color color;
+
+    private int movementDirection = 1;
 
     public Balloon(double xPos, double yPos, AbstractSpriteWorld world) {
         this(xPos, yPos, Color.getHSBColor(BalloonGame.RANDOM.nextFloat(), 1f, 1f), world);
@@ -31,5 +33,22 @@ public class Balloon extends Sprite {
         triangle.ypoints = new int[] {offset + 10, offset + 0, offset + 10};
         triangle.npoints = 3;
         g.fillPolygon(triangle);
+    }
+
+    @Override
+    public void update(double deltaTime, double time) {
+        super.update(deltaTime, time);
+        this.move(deltaTime);
+    }
+
+    private void move(double deltaTime) {
+        double newPos = this.getY() + movementDirection * deltaTime * SPEED;
+        if (newPos < 0) {
+            movementDirection = 1;
+        } else if (newPos > this.getAbstractSpriteWorld().getHeight()) {
+            movementDirection = -1;
+        }
+        newPos = this.getY() + movementDirection * deltaTime * SPEED;
+        this.setY(newPos);
     }
 }
