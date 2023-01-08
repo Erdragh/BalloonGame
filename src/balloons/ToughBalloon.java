341be44e22ecc989ed4c3e36fc44a00b5171b65a
Jan Bayer <enderjanyt@gmail.com>
2023-01-08 13:28:31 +0100

    add comment to ToughBalloon.java

--- src/balloons/ToughBalloon.java
+++ src/balloons/ToughBalloon.java
@@ -10,7 +10,11 @@ public class ToughBalloon extends Balloon {
 
     @Override
     public boolean onDeath(BalloonGame game) {
+        // randomize the color of the balloon to signify that
+        // it has been hit.
         this.randomizeColor();
+        // only return true and therefore remove the balloon if
+        // the newly calculated health is below 1.
         return --health < 1;
     }
 }
